745334c743da562b8293d8aaca8fca40c81a05d4
Aswin Reji <aswinreji.004@gmail.com>
2025-04-13 14:16:55 +0000

    feat: Added app component to try the layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
-    <></>
-  )
+    <>
+      <div className="flex items-center justify-center min-h-screen text-center">
+        <div>
+          <h1 className="text-4xl font-bold mb-4">Sentence Construction</h1>
+          <p className="text-lg text-gray-400 mb-4">
+            Select the words to complete the sentence by arranging <br />
+            the provided options in the right order
+          </p>
+        </div>
+
+        <div>
+          <h1>Time Per Question</h1>
+          <p>30 sec</p>
+        </div>
+        <div>
+          <h1>Total Questions </h1>
+          <p>10</p>
+        </div>
+        <div>
+          <h1>Coins</h1>
+          <p>0</p>
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
+
